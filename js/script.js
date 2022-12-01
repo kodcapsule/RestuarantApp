6c240c160e1f0e3df12a63d152b62ca644ffb8f6
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2022-12-01 23:23:31 +0000

    styled the dishes section

--- js/script.js
+++ js/script.js
@@ -29,13 +29,33 @@ formClose.addEventListener("click", (e) => {
   searchForm.classList.remove("active");
 });
 
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
 // var swiper = new Swiper(".home-swiper", {
-//   slidesPerView: 3,
-//   centeredSlides: true,
 //   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 700,
+//     disableOnInteraction: false,
+//   },
 //   pagination: {
 //     el: ".swiper-pagination",
-//     type: "fraction",
+//     clickable: true,
 //   },
-//   loop: true,
+//   loo: tru,
 // });
