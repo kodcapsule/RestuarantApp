fb8822b9db648df3b3f3ec0e9100d05dde2c0939
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2022-12-04 12:25:58 +0000

    added customer testimonials and swiper js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from "swiper";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 const menu = document.querySelector(".menu");
 const menubars = document.querySelector(".menu-bars");
 const menuclose = document.querySelector(".close");
@@ -29,22 +36,22 @@ formClose.addEventListener("click", (e) => {
   searchForm.classList.remove("active");
 });
 
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+// var swiper = new Swiper(".mySwiper", {
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
 
 // var swiper = new Swiper(".home-swiper", {
 //   spaceBetween: 30,
@@ -58,4 +65,15 @@ var swiper = new Swiper(".mySwiper", {
 //     clickable: true,
 //   },
 //   loo: tru,
+// });
+
+// var swiper = new Swiper(".review-slider", {
+//   spaceBetween: 20,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 700,
+//     disableOnInteraction: false,
+//   },
+
+//   loop: true,
 // });
