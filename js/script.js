36af6534abcf78d1f50c80e4180582a39e6cb55a
Kuseh Simon Wewoliamo <kusehsw@gmail.com>
2022-12-10 21:58:29 +0000

    added oder section

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // core version + navigation, pagination modules:
-import Swiper, { Navigation, Pagination } from "swiper";
+// import Swiper, { Navigation, Pagination } from "swiper";
 // import Swiper and modules styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
 
 const menu = document.querySelector(".menu");
 const menubars = document.querySelector(".menu-bars");
